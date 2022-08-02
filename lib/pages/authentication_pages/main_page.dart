8541e46bc3e0ac2cea71ff77f117e9aff874ff08
baydemirytu <baydemir198@gmail.com>
2022-08-02 17:12:07 +0300

    Drawer update

--- lib/pages/authentication_pages/main_page.dart
+++ lib/pages/authentication_pages/main_page.dart
@@ -2,8 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sanal_portfoy_yonetim_simulasyonu/pages/authentication_pages/verify_email_page.dart';
 import 'authentication_page.dart';
-import '../home_page.dart';
-import 'login_page.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
