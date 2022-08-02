0532166675446312d87606577abc3a5edae2c6af
baydemirytu <baydemir198@gmail.com>
2022-08-02 11:59:59 +0300

    Firestore version fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,9 +12,6 @@ import 'package:sanal_portfoy_yonetim_simulasyonu/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
