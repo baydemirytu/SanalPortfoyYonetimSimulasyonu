3ae3f8764f374b94e5bebca734f17f59ddb1e4c2
baydemirytu <baydemir198@gmail.com>
2022-08-02 20:49:45 +0300

    Date picker entry is banned, some fixes

--- lib/tahvil_ve_bono_hesaplama.dart
+++ lib/tahvil_ve_bono_hesaplama.dart
@@ -33,11 +33,6 @@ class _TahvilVeBonoState extends State<TahvilVeBono> {
       DateTime dt = DateTime(DateTime.now().year, DateTime.now().month,
           DateTime.now().day + days + 1);
       dateInput.text = DateFormat('dd-MM-yyyy').format(dt);
-      print(dt.year.toString() +
-          '-' +
-          dt.month.toString() +
-          '-' +
-          dt.day.toString());
     } else if (days != null && days >= 100000) {
       validateDays = false;
     }
@@ -49,125 +44,127 @@ class _TahvilVeBonoState extends State<TahvilVeBono> {
     remaningDaysInput.text = dt.difference(DateTime.now()).inDays.toString();
   }
 
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            'Bono ve Tahvil Hesaplama',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-            decoration: InputDecoration(
-              labelText: 'Ana Para',
-              hintText: 'Örnek: 100.000',
-              icon: const Icon(Icons.attach_money),
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(width: 3, color: Colors.blue),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(width: 3, color: Colors.red),
-                borderRadius: BorderRadius.circular(15),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Bono ve Tahvil Hesaplama',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: InputDecoration(
+                labelText: 'Ana Para',
+                hintText: 'Örnek: 100.000',
+                icon: const Icon(Icons.attach_money),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.blue),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.red),
+                  borderRadius: BorderRadius.circular(15),
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              icon: const Icon(Icons.percent),
-              labelText: 'Yıllık Basit Faiz Oranı (%)',
-              hintText: 'Örnek: 14',
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(width: 3, color: Colors.blue),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(width: 3, color: Colors.red),
-                borderRadius: BorderRadius.circular(15),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                icon: const Icon(Icons.percent),
+                labelText: 'Yıllık Basit Faiz Oranı (%)',
+                hintText: 'Örnek: 14',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.blue),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.red),
+                  borderRadius: BorderRadius.circular(15),
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          TextField(
-            controller: remaningDaysInput,
-            keyboardType: TextInputType.number,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-            decoration: InputDecoration(
-              errorText:
-                  !validateDays ? 'Enter a day value less than 100000' : null,
-              icon: const Icon(Icons.hourglass_bottom),
-              hintText: 'Örnek: 50',
-              labelText: 'Vadeye Kalan Gün',
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(width: 3, color: Colors.blue),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(width: 3, color: Colors.red),
-                borderRadius: BorderRadius.circular(15),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextField(
-            controller: dateInput,
-            decoration: InputDecoration(
-              icon: const Icon(Icons.date_range_sharp),
-              hintText: 'Örnek: 01.01.2023',
-              labelText: 'Ya Da Vade Bitiş Tarihi',
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(width: 3, color: Colors.blue),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(width: 3, color: Colors.red),
-                borderRadius: BorderRadius.circular(15),
+            const SizedBox(height: 20),
+            TextField(
+              controller: remaningDaysInput,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: InputDecoration(
+                errorText:
+                    !validateDays ? 'Enter a day value less than 100000' : null,
+                icon: const Icon(Icons.hourglass_bottom),
+                hintText: 'Örnek: 50',
+                labelText: 'Vadeye Kalan Gün',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.blue),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.red),
+                  borderRadius: BorderRadius.circular(15),
+                ),
               ),
             ),
-            readOnly: true,
-            onTap: () async {
-              DateTime? pickedDate = await showDatePicker(
-                context: context,
-                initialDate: DateTime(DateTime.now().year, DateTime.now().month,
-                    DateTime.now().day + 1), // tomorrow
-                firstDate: DateTime(DateTime.now().year, DateTime.now().month,
-                    DateTime.now().day + 1), // tomorrow
-                lastDate: DateTime(2099),
-              );
-
-              if (pickedDate != null) {
-                String formattedDate =
-                    DateFormat('yyyy-MM-dd').format(pickedDate);
-
-                setState(
-                  () {
-                    dateInput.text =
-                        formattedDate; //set output date to TextField value.
-                  },
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: dateInput,
+              decoration: InputDecoration(
+                icon: const Icon(Icons.date_range_sharp),
+                hintText: 'Örnek: 01.01.2023',
+                labelText: 'Ya Da Vade Bitiş Tarihi',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.blue),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.red),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              readOnly: true,
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                  initialEntryMode: DatePickerEntryMode.calendarOnly,
+                  context: context,
+                  initialDate: DateTime(DateTime.now().year,
+                      DateTime.now().month, DateTime.now().day + 1), // tomorrow
+                  firstDate: DateTime(DateTime.now().year, DateTime.now().month,
+                      DateTime.now().day + 1), // tomorrow
+                  lastDate: DateTime(2099),
                 );
-              } else {
-                print("Date is not selected");
-              }
-            },
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Calculate'),
-          ),
-        ],
+
+                if (pickedDate != null) {
+                  String formattedDate =
+                      DateFormat('yyyy-MM-dd').format(pickedDate);
+
+                  setState(
+                    () {
+                      dateInput.text =
+                          formattedDate; //set output date to TextField value.
+                    },
+                  );
+                }
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Calculate'),
+            ),
+          ],
+        ),
       ),
     );
   }
