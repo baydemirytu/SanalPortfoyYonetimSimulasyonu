c56059908ee678faaa77ca652cf661ce22136c47
emreduzakin <72689359+emreduzakin@users.noreply.github.com>
2022-08-08 13:32:17 +0300

    More currencies added

--- lib/pages/prices_pages/loading_screen_prices.dart
+++ lib/pages/prices_pages/loading_screen_prices.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:sanal_portfoy_yonetim_simulasyonu/pages/prices_pages/networking.dart';
 import 'package:sanal_portfoy_yonetim_simulasyonu/pages/prices_pages/prices_screen.dart';
 
+//Bazi seyler hard coded, cunku api yi degistirmemiz gerekecek.
+//Duzgun bir api buldukltan sonra kalabaligi alacagim.
+
 //This page loads data
 class PricesLoading extends StatefulWidget {
   const PricesLoading({Key? key}) : super(key: key);
@@ -26,9 +29,52 @@ class _PricesLoadingState extends State<PricesLoading> {
     myNetworkHelper = NetworkHelper(
         'https://api.exchangerate.host/convert?from=$choosenPair&to=TRY');
     var gbpData = await myNetworkHelper.requestData();
+    /*****/
+
+    choosenPair = 'AED';
+    myNetworkHelper = NetworkHelper(
+        'https://api.exchangerate.host/convert?from=$choosenPair&to=TRY');
+    var aedData = await myNetworkHelper.requestData();
+    choosenPair = 'AUD';
+    myNetworkHelper = NetworkHelper(
+        'https://api.exchangerate.host/convert?from=$choosenPair&to=TRY');
+    var audData = await myNetworkHelper.requestData();
+    choosenPair = 'CAD';
+    myNetworkHelper = NetworkHelper(
+        'https://api.exchangerate.host/convert?from=$choosenPair&to=TRY');
+    var cadData = await myNetworkHelper.requestData();
+    choosenPair = 'CHF';
+    myNetworkHelper = NetworkHelper(
+        'https://api.exchangerate.host/convert?from=$choosenPair&to=TRY');
+    var chfData = await myNetworkHelper.requestData();
+    choosenPair = 'DKK';
+    myNetworkHelper = NetworkHelper(
+        'https://api.exchangerate.host/convert?from=$choosenPair&to=TRY');
+    var dkkData = await myNetworkHelper.requestData();
+    choosenPair = 'JPY';
+    myNetworkHelper = NetworkHelper(
+        'https://api.exchangerate.host/convert?from=$choosenPair&to=TRY');
+    var jpyData = await myNetworkHelper.requestData();
+    choosenPair = 'KWD';
+    myNetworkHelper = NetworkHelper(
+        'https://api.exchangerate.host/convert?from=$choosenPair&to=TRY');
+    var kwdData = await myNetworkHelper.requestData();
+    choosenPair = 'NOK';
+    myNetworkHelper = NetworkHelper(
+        'https://api.exchangerate.host/convert?from=$choosenPair&to=TRY');
+    var nokData = await myNetworkHelper.requestData();
+    choosenPair = 'SAR';
+    myNetworkHelper = NetworkHelper(
+        'https://api.exchangerate.host/convert?from=$choosenPair&to=TRY');
+    var sarData = await myNetworkHelper.requestData();
+    choosenPair = 'SEK';
+    myNetworkHelper = NetworkHelper(
+        'https://api.exchangerate.host/convert?from=$choosenPair&to=TRY');
+    var sekData = await myNetworkHelper.requestData();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return PriceScreen(usdData, eurData, gbpData);
+      return PriceScreen(usdData, eurData, gbpData, aedData, audData, cadData,
+          chfData, dkkData, jpyData, kwdData, nokData, sarData, sekData);
     }));
   }
 
