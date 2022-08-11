121cfc9f768c54a0e76629e2b6ebe8186973a81d
baydemirytu <baydemir198@gmail.com>
2022-08-11 16:52:02 +0300

    Other currency pairs are added

--- lib/pages/calculation_pages/doviz_hesaplama.dart
+++ lib/pages/calculation_pages/doviz_hesaplama.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sanal_portfoy_yonetim_simulasyonu/pages/calculation_pages/vadeli_mevduat_hesaplama.dart';
+import 'package:sanal_portfoy_yonetim_simulasyonu/pages/portfolio_page.dart'
+    as prt;
 import 'package:sanal_portfoy_yonetim_simulasyonu/pages/prices_pages/networking.dart';
 
 class DovizHesaplama extends StatefulWidget {
@@ -14,10 +16,30 @@ class _DovizHesaplamaState extends State<DovizHesaplama> {
   var euroData;
   var dolarData;
   var poundData;
+  var aedData;
+  var audData;
+  var cadData;
+  var chfData;
+  var dkkData;
+  var jpyData;
+  var kwdData;
+  var nokData;
+  var sarData;
+  var sekData;
 
   var euro = 18.25;
   var dolar = 17.931;
   var pound = 21.669;
+  var aed = 4.86;
+  var aud = 12.55;
+  var cad = 14.0;
+  var chf = 18.95;
+  var dkk = 2.47;
+  var jpy = 0.1342;
+  var kwd = 58.0;
+  var nok = 1.87;
+  var sar = 4.73;
+  var sek = 1.78;
   double lira = 1;
 
   TextEditingController miktarController = TextEditingController();
@@ -40,35 +62,90 @@ class _DovizHesaplamaState extends State<DovizHesaplama> {
         NetworkHelper('https://api.exchangerate.host/convert?from=GBP&to=TRY');
     poundData = await helper.requestData();
     pound = poundData['result'];
+
+    helper =
+        NetworkHelper('https://api.exchangerate.host/convert?from=AED&to=TRY');
+    aedData = await helper.requestData();
+    aed = aedData['result'];
+
+    helper =
+        NetworkHelper('https://api.exchangerate.host/convert?from=AUD&to=TRY');
+    audData = await helper.requestData();
+    aud = audData['result'];
+
+    helper =
+        NetworkHelper('https://api.exchangerate.host/convert?from=CAD&to=TRY');
+    cadData = await helper.requestData();
+    cad = cadData['result'];
+
+    helper =
+        NetworkHelper('https://api.exchangerate.host/convert?from=CHF&to=TRY');
+    chfData = await helper.requestData();
+    chf = chfData['result'];
+
+    helper =
+        NetworkHelper('https://api.exchangerate.host/convert?from=DKK&to=TRY');
+    dkkData = await helper.requestData();
+    dkk = dkkData['result'];
+
+    helper =
+        NetworkHelper('https://api.exchangerate.host/convert?from=JPY&to=TRY');
+    jpyData = await helper.requestData();
+    jpy = jpyData['result'];
+
+    helper =
+        NetworkHelper('https://api.exchangerate.host/convert?from=KWD&to=TRY');
+    kwdData = await helper.requestData();
+    kwd = kwdData['result'];
+
+    helper =
+        NetworkHelper('https://api.exchangerate.host/convert?from=NOK&to=TRY');
+    nokData = await helper.requestData();
+    nok = nokData['result'];
+
+    helper =
+        NetworkHelper('https://api.exchangerate.host/convert?from=SAR&to=TRY');
+    sarData = await helper.requestData();
+    sar = sarData['result'];
+
+    helper =
+        NetworkHelper('https://api.exchangerate.host/convert?from=SEK&to=TRY');
+    sekData = await helper.requestData();
+    sek = sekData['result'];
   }
 
-  Icon liraIcon = const Icon(
-    Icons.currency_lira_outlined,
-    color: Colors.green,
-  );
-  Icon usdIcon = const Icon(
-    Icons.attach_money_outlined,
-    color: Colors.green,
-  );
-  Icon euroIcon = const Icon(
-    Icons.euro_symbol_outlined,
-    color: Colors.green,
-  );
-  Icon poundIcon = const Icon(
-    Icons.currency_pound_outlined,
-    color: Colors.green,
-  );
-
-  Icon dovizIcon = const Icon(
-    Icons.currency_lira_outlined,
-    color: Colors.green,
-  );
+  final Map<String?, String> currencyEmojis = {
+    'USD': '🇺🇸',
+    'EUR': '🇪🇺',
+    'GBP': '🇬🇧',
+    'AED': '🇦🇪',
+    'AUD': '🇦🇺',
+    'CAD': '🇨🇦',
+    'CHF': '🇨🇭',
+    'DKK': '🇩🇰',
+    'JPY': '🇯🇵',
+    'KWD': '🇰🇼',
+    'NOK': '🇳🇴',
+    'SAR': '🇸🇦',
+    'SEK': '🇸🇪',
+    'TRY': '🇹🇷'
+  };
 
   List<DropdownMenuItem<String>> dovizList = <String>[
     'TRY',
     'USD',
     'EUR',
-    'GBP'
+    'GBP',
+    'AED',
+    'AUD',
+    'CAD',
+    'CHF',
+    'DKK',
+    'JPY',
+    'KWD',
+    'NOK',
+    'SAR',
+    'SEK',
   ].map<DropdownMenuItem<String>>((String value) {
     return DropdownMenuItem<String>(
       value: value,
@@ -134,7 +211,7 @@ class _DovizHesaplamaState extends State<DovizHesaplama> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      dovizIcon,
+                      Text('${currencyEmojis[dropdownValue]}'),
                       Container(
                         padding: const EdgeInsets.symmetric(
                             vertical: 4, horizontal: 8),
@@ -155,24 +232,51 @@ class _DovizHesaplamaState extends State<DovizHesaplama> {
 
                               switch (dropdownValue) {
                                 case 'TRY':
-                                  dovizIcon = liraIcon;
                                   selectedDoviz = lira;
                                   break;
                                 case 'USD':
-                                  dovizIcon = usdIcon;
                                   selectedDoviz = dolar;
                                   break;
                                 case 'EUR':
-                                  dovizIcon = euroIcon;
                                   selectedDoviz = euro;
                                   break;
                                 case 'GBP':
-                                  dovizIcon = poundIcon;
                                   selectedDoviz = pound;
                                   break;
+                                case 'AED':
+                                  selectedDoviz = aed;
+                                  break;
+                                case 'AUD':
+                                  selectedDoviz = aud;
+                                  break;
+                                case 'CAD':
+                                  selectedDoviz = cad;
+                                  break;
+                                case 'CHF':
+                                  selectedDoviz = chf;
+                                  break;
+                                case 'DKK':
+                                  selectedDoviz = dkk;
+                                  break;
+                                case 'JPY':
+                                  selectedDoviz = jpy;
+                                  break;
+                                case 'KWD':
+                                  selectedDoviz = kwd;
+                                  break;
+                                case 'NOK':
+                                  selectedDoviz = nok;
+                                  break;
+                                case 'SAR':
+                                  selectedDoviz = sar;
+                                  break;
+                                case 'SEK':
+                                  selectedDoviz = sek;
+                                  break;
+
                                 default:
                                   {
-                                    dovizIcon = liraIcon;
+                                    selectedDoviz = lira;
                                   }
                               }
                             });
@@ -188,30 +292,42 @@ class _DovizHesaplamaState extends State<DovizHesaplama> {
             const SizedBox(
               height: 20,
             ),
-            ListTile(
-                title: const Text('TRY'),
-                leading: const Text(
-                  '🇹🇷',
-                  style: TextStyle(fontSize: 25),
-                ),
-                trailing:
-                    Text((miktar! * selectedDoviz / lira).toStringAsFixed(4))),
-            ListTile(
-                leading: const Text('🇺🇸', style: TextStyle(fontSize: 25)),
-                title: const Text('USD'),
-                trailing:
-                    Text((miktar! * selectedDoviz / dolar).toStringAsFixed(4))),
-            ListTile(
-                leading: const Text('🇪🇺', style: TextStyle(fontSize: 25)),
-                title: const Text('EUR'),
-                trailing:
-                    Text((miktar! * selectedDoviz / euro).toStringAsFixed(4))),
-            ListTile(
-                leading: const Text('🇬🇧', style: TextStyle(fontSize: 25)),
-                title: const Text('GBP'),
-                trailing:
-                    Text((miktar! * selectedDoviz / pound).toStringAsFixed(4))),
+            buildListTile('🇹🇷', 'TRY', lira),
+            buildListTile('🇺🇸', 'USD', dolar),
+            buildListTile('🇪🇺', 'EUR', euro),
+            buildListTile('🇬🇧', 'GBP', pound),
+            buildListTile('🇦🇪', 'AED', aed),
+            buildListTile('🇦🇺', 'AUD', aud),
+            buildListTile('🇨🇦', 'CAD', cad),
+            buildListTile('🇨🇭', 'CHF', chf),
+            buildListTile('🇩🇰', 'DKK', dkk),
+            buildListTile('🇯🇵', 'JPY', jpy),
+            buildListTile('🇰🇼', 'KWD', kwd),
+            buildListTile('🇳🇴', 'NOK', nok),
+            buildListTile('🇸🇦', 'SAR', sar),
+            buildListTile('🇸🇪', 'SEK', sek),
           ],
+        ),
+      ),
+    );
+  }
+
+  Padding buildListTile(String flag, String country, double currency) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 1, color: Colors.cyan),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: ListTile(
+          leading: Text(
+            flag,
+            style: const TextStyle(fontSize: 25),
+          ),
+          title: Text(country),
+          trailing:
+              Text((miktar! * selectedDoviz / currency).toStringAsFixed(4)),
         ),
       ),
     );
