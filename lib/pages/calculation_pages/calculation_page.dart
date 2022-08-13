ab987a94b14ff03c666f65b7fea4061d33bb9b40
baydemirytu <baydemir198@gmail.com>
2022-08-13 08:58:31 +0300

    Navigate to Home Page when back button is pressed

--- lib/pages/calculation_pages/calculation_page.dart
+++ lib/pages/calculation_pages/calculation_page.dart
@@ -3,6 +3,7 @@ import 'package:sanal_portfoy_yonetim_simulasyonu/constants/widgets/app_bar_draw
 import 'package:sanal_portfoy_yonetim_simulasyonu/pages/calculation_pages/doviz_hesaplama.dart';
 import 'package:sanal_portfoy_yonetim_simulasyonu/pages/calculation_pages/vadeli_mevduat_hesaplama.dart';
 
+import '../home_page.dart';
 import 'tahvil_ve_bono_hesaplama.dart';
 
 class CalculationPage extends StatefulWidget {
@@ -15,50 +16,60 @@ class CalculationPage extends StatefulWidget {
 class _CalculationPageState extends State<CalculationPage> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        drawer: const AppBarDrawer(),
-        appBar: AppBar(
-          shadowColor: Colors.white,
-          elevation: 2,
-          title: const Text('Hesaplama Ekranı'),
-          centerTitle: true,
-          bottom: const TabBar(
-              isScrollable: true,
-              indicatorWeight: 4,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorColor: Color.fromARGB(255, 63, 101, 133),
-              tabs: [
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  child: Text(
-                    'Döviz Hesaplama',
-                    style: TextStyle(fontSize: 17),
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const HomePage(),
+            ));
+        return false;
+      },
+      child: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          drawer: const AppBarDrawer(),
+          appBar: AppBar(
+            shadowColor: Colors.white,
+            elevation: 2,
+            title: const Text('Hesaplama Ekranı'),
+            centerTitle: true,
+            bottom: const TabBar(
+                isScrollable: true,
+                indicatorWeight: 4,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorColor: Color.fromARGB(255, 63, 101, 133),
+                tabs: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    child: Text(
+                      'Döviz Hesaplama',
+                      style: TextStyle(fontSize: 17),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  child: Text(
-                    'Vadeli Mevduat',
-                    style: TextStyle(fontSize: 17),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    child: Text(
+                      'Vadeli Mevduat',
+                      style: TextStyle(fontSize: 17),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  child: Text(
-                    'Bono Ve Tahvil',
-                    style: TextStyle(fontSize: 17),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    child: Text(
+                      'Bono Ve Tahvil',
+                      style: TextStyle(fontSize: 17),
+                    ),
                   ),
-                ),
-              ]),
-        ),
-        body: const TabBarView(
-          children: <Widget>[
-            DovizHesaplama(),
-            VadeliMevduatHesaplama(),
-            TahvilVeBono(),
-          ],
+                ]),
+          ),
+          body: const TabBarView(
+            children: <Widget>[
+              DovizHesaplama(),
+              VadeliMevduatHesaplama(),
+              TahvilVeBono(),
+            ],
+          ),
         ),
       ),
     );
