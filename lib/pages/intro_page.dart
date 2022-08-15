09da5cb0d794bea40f7a911bdb5048475493570a
baydemirytu <baydemir198@gmail.com>
2022-08-15 12:35:58 +0300

    introduction screen arranged

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -15,15 +15,100 @@ class IntroductionPage extends StatelessWidget {
     return [
       PageViewModel(
         image: Image.asset("assets/images/yapi_kredi_logo.png"),
-        title: "Page 1",
-        body: "Page 1's body",
-        footer: const Text("Page 1's footer"),
+        title: "Sabit Getirili Menkul Kıymetler\nStaj Projesi",
+        body: "",
+        footer: const Text(""),
       ),
       PageViewModel(
         image: Image.asset("assets/images/yapi_kredi_logo.png"),
-        title: "Page 2",
-        body: "Page 2's body",
-        footer: const Text("Page 2's footer"),
+        title: "Üyeler",
+        body: "Mentörler",
+        footer: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text("Oğuzhan Şengül - Danışman Yazılım Mühendisi")
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [Text("Mehmet Abay - Danışman İş Analisti")],
+            )
+          ],
+        ),
+      ),
+      PageViewModel(
+        image: Image.asset("assets/images/yapi_kredi_logo.png"),
+        title: "Üyeler",
+        body: "Stajyerler",
+        footer: Column(
+          children: const [
+            Text("Bahadır Patlak - İş Analisti Stajyeri",
+                textAlign: TextAlign.center),
+            Text("Barış Söke - Yazılım Mühendisi Stajyeri",
+                textAlign: TextAlign.center),
+            Text("Batuhan Ahmet Aydemir - Yazılım Mühendisi Stajyeri",
+                textAlign: TextAlign.center),
+            Text("Bora Polater - İş Analisti Stajyeri",
+                textAlign: TextAlign.center),
+            Text("Doğu Özcan - Yazılım Mühendisi Stajyeri",
+                textAlign: TextAlign.center),
+            Text("Emre Düzakın - Yazılım Mühendisi Stajyeri",
+                textAlign: TextAlign.center),
+            Text("Muzaffer Mert Akkan - Yazılım Mühendisi Stajyeri",
+                textAlign: TextAlign.center),
+          ],
+        ),
+      ),
+      PageViewModel(
+        image: Image.asset("assets/images/yapi_kredi_logo.png"),
+        title: "Sanal Portföy Uygulaması",
+        body: "Üyelik Aç",
+        footer: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text('1) Email hesabınızla kayıt olun'),
+            Text('2) Başlangıç bakiyenizi kendiniz belirleyin'),
+            Text('3) Spam klasörüne düşebilecek olan maildeki linke tıklayın'),
+            Text(
+                '4) Diğer üyeler arasında kar yarışmasında sıralamanızı görün'),
+            Text('5) Sanal Portföy\'ünüzü yönetmeye başlayın 🚀')
+          ],
+        ),
+      ),
+      PageViewModel(
+        image: Image.asset("assets/images/yapi_kredi_logo.png"),
+        title: "Sanal Portföy Uygulaması",
+        body: "Hesaplama Araçları",
+        footer: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text('1) Tahvil'),
+            Text('2) Bono'),
+            Text('3) Vadeli Mevduat'),
+            Text('4) Döviz Kurları'),
+            Text('hesaplamalarınızı kolaylıkla yapın')
+          ],
+        ),
+      ),
+      PageViewModel(
+        image: Image.asset("assets/images/yapi_kredi_logo.png"),
+        title: "Sanal Portföy Uygulaması",
+        body: "Fiyatlar",
+        footer: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text('1) Döviz kurlarını görüntüleyin, alın, satın'),
+            Text('2) Kripto paraları görüntüleyin, alın, satın'),
+          ],
+        ),
+      ),
+      PageViewModel(
+        image: Image.asset("assets/images/yapi_kredi_logo.png"),
+        title: "Hazır mısın?",
+        body: "",
+        footer: const Text(""),
       ),
     ];
   }
@@ -45,8 +130,9 @@ class IntroductionPage extends StatelessWidget {
         showSkipButton: true,
         showBackButton: false,
         showNextButton: false,
-        skip: const Text("Skip"),
-        done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+        skip: const Text("Atla"),
+        done: const Text("Hem de nasıl",
+            style: TextStyle(fontWeight: FontWeight.w600)),
         onDone: () {
           saveDone();
           Navigator.pushReplacement(
